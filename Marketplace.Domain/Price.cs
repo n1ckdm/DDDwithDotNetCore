f6a2b0a0b557815775a26577e62a634ed83eefec
Nick Martin <nicholas.martin@tharsus.co.uk>
2022-05-20 23:30:58 +0100

    page 135

--- Marketplace.Domain/Price.cs
+++ Marketplace.Domain/Price.cs
@@ -9,5 +9,14 @@ namespace Marketplace.Domain
             if (amount < 0)
                 throw new ArgumentException("Price cannot be nagative", nameof(amount));
         }
+
+        public new static Price FromDecimal(
+            decimal amount,
+            string currency,
+            ICurrencyLookup currencyLookup) => new Price(amount, currency, currencyLookup);
+        public new static Price FromString(
+            string amount,
+            string currency,
+            ICurrencyLookup currencyLookup) => new Price(decimal.Parse(amount), currency, currencyLookup);
     }
 }
