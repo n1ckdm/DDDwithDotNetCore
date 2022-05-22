b93ea664af35557e76f9f5852b0c4c605301b2c5
Nick Martin <nicholas.martin@tharsus.co.uk>
2022-05-22 10:23:49 +0100

    page 160

---
+++ Marketplace/Api/ClassifiedAdsCommandsApi.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Marketplace.Api
+{
+    [Route("/ad")]
+    public class ClassifiedAdsCommandsApi : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post(
+            Contracts.ClassifiedAds.V1.Create request
+        )
+        {
+            // Handle the request here...
+            return Ok();
+        }
+    }
+}
--- Marketplace/Program.cs
+++ Marketplace/Program.cs
@@ -1,6 +1,34 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using static System.Environment;
+using static System.Reflection.Assembly;
 
-app.MapGet("/", () => "Hello World!");
+namespace Marketplace
+{
+    static class Program
+    {
+        static Program() => Path.GetDirectoryName(GetEntryAssembly().Location);
 
-app.Run();
+        public static void Main(string[] args)
+        {
+            var configuration = BuildConfiguration(args);
+            ConfigureWebHost(configuration).Build().Run();
+        }
+
+        private static IConfiguration BuildConfiguration(String[] args)
+            => new ConfigurationBuilder()
+                .SetBasePath(CurrentDirectory)
+                .Build();
+
+        private static IWebHostBuilder ConfigureWebHost(
+            IConfiguration configuration
+        ) => new WebHostBuilder()
+                .UseStartup<Startup>()
+                .UseConfiguration(configuration)
+                .ConfigureServices(services => services.AddSingleton(configuration))
+                .UseContentRoot(CurrentDirectory)
+                .UseKestrel();
+    }
+}
